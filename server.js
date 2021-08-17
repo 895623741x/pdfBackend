fd8c886293ad3e962d09c3120771496994322495
895623741x <tommyhsu69@gmail.com>
2021-08-17 03:06:22 -0700

    third commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 const PORT = (process.env.PORT = 8000);
 app.use(cors());
-app.use(express.static("/public"));
+app.use(express.static("public"));
 
 var list = "";
 const { exec } = require("child_process");
