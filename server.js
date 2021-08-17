87ce5f482b4933de917e007c7ee7b0ea739ad19e
895623741x <tommyhsu69@gmail.com>
2021-08-17 12:39:44 -0700

    fix upload

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 var list = "";
