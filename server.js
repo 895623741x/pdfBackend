9b713f2b0a19a4e3a5aada6583e0d1791226edf4
895623741x <tommyhsu69@gmail.com>
2021-08-17 04:56:41 -0700

    change PORT

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-const PORT = (process.env.PORT = 8000);
+const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.static("public"));
 
